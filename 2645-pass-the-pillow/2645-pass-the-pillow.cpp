ff16b2c59a3515484e67dd1800ad61331dcff0e6
Prateek <109753518+Prateek-rajput-007@users.noreply.github.com>
2023-10-18 22:35:04 +0530

    Time: 2 ms (50.78%), Space: 6.4 MB (17.51%) - LeetHub

--- 2645-pass-the-pillow/2645-pass-the-pillow.cpp
+++ 2645-pass-the-pillow/2645-pass-the-pillow.cpp
@@ -1,40 +1,18 @@
 class Solution {
 public:
     int passThePillow(int n, int time) {
-        vector<int>v1; 
-        vector<int>v2;
-
-        int i=1;
-        while(i<=n){
-            v1.push_back(i++);
-        } 
-
-        i=n-1;
-        while(i>0){
-            v2.push_back(i--);
-        }
-
-        vector<int>ans;
+        int index=1;
         int cnt=1;
+        while(time--){
 
-        for(int it:v1)ans.push_back(it);
-        for(int it:v2)ans.push_back(it);
+          if(index==1)
+              cnt=1;
+            if(index==n)
+                cnt=-1;
+            
+          index+=cnt;
 
-        while(cnt<=time){
-
-            int i=2;
-            while(i<=n){
-                ans.push_back(i++);
-                cnt++;
-            } 
-
-            i=n-1;
-            while(i>0){
-                ans.push_back(i--);
-                cnt++;
-            }
-        }
-
-        return ans[time];
+ }
+        return index;
     }
 };
