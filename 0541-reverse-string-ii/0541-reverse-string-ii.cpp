3bbca8d31ce57fa92bea3cdbf990697555f6f794
Prateek <109753518+Prateek-rajput-007@users.noreply.github.com>
2023-10-18 11:47:49 +0530

    Time: 3 ms (88.6%), Space: 7.6 MB (65.52%) - LeetHub

---
+++ 0541-reverse-string-ii/0541-reverse-string-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    
+    //1. reverse k elements and  jump 2k elements
+    string reverseStr(string s, int k) {
+        
+        for (int i = 0; i < s.size(); i += 2*k) {
+            reverse(s.begin()+i, min(s.begin()+i+k, s.end()));
+        }
+        return s;
+    }
+};
