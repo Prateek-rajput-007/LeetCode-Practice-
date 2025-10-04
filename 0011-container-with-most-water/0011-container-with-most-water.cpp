af9d4894eeb7af278ddb32d9d3919242a689225e
Prateek <109753518+Prateek-rajput-007@users.noreply.github.com>
2025-10-04 11:37:46 +0530

    Time: 0 ms (100%), Space: 62.9 MB (48.17%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -5,9 +5,12 @@ public:
         int ans = 0;
         int s = 0;
         int e = height.size() - 1;
-        
+        int w=0,h=0,area=0;
         while(s < e) {
-            ans = max(ans, (min(height[s], height[e]) * (e - s)));
+            w = e-s;
+            h = min(height[s],height[e]);
+            area = w*h;
+            ans = max(ans,area);
             if(height[s] < height[e])
                 s++;
             else 
