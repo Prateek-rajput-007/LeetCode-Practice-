7c599aa2715e0502dbd5668ce81f03a5dc8a12ce
Prateek <109753518+Prateek-rajput-007@users.noreply.github.com>
2025-10-06 16:39:22 +0530

    Time: 121 ms (5.05%), Space: 12.6 MB (93.11%) - LeetHub

---
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int n,m;
+    vector<vector<int>>dir ={{-1,0},{1,0},{0,-1},{0,1}};
+    int ans=INT_MAX;
+    void solve(int i,int j,vector<vector<int>>& grid,vector<vector<int>>& dis,int d){
+
+        if(i>=n || i<0 || j>=m || j<0){
+            return ;
+        }
+        int newMax = max(d, grid[i][j]);
+        if(dis[i][j]<=newMax){
+            return ;
+        }
+        dis[i][j]=newMax;
+
+        if(i==n-1 && j==m-1){
+            ans=min(ans,newMax);
+            return ;
+        }
+        solve(i-1,j,grid,dis,newMax);
+        solve(i+1,j,grid,dis,newMax);
+        solve(i,j-1,grid,dis,newMax);
+        solve(i,j+1,grid,dis,newMax);
+
+
+    }
+    int swimInWater(vector<vector<int>>& grid) {
+        n=grid.size();
+        m=grid[0].size();
+        vector<vector<int>>dis(n,vector<int>(m,1e9));
+        solve(0,0,grid,dis,grid[0][0]);
+        return ans;
+    }
+};
