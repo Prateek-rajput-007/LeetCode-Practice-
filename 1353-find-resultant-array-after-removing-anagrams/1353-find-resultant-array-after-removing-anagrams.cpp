d495a14a2cd918a53dabab6df92c1a267a17cc9e
Prateek <109753518+Prateek-rajput-007@users.noreply.github.com>
2025-10-13 09:50:23 +0530

    Time: 0 ms (100%), Space: 16 MB (75.29%) - LeetHub

---
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool check(string a, string b) {
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+
+        return a == b;
+    }
+    vector<string> removeAnagrams(vector<string>& words) {
+
+        vector<string> ans;
+        ans.push_back(words[0]);
+        int n = words.size();
+        for (int i = 1; i < n; i++) {
+
+            if (!check(words[i], ans.back())) {
+                ans.push_back(words[i]);
+            }
+        }
+
+        return ans;
+    }
+};
